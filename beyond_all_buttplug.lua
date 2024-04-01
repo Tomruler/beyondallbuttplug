12d28323dc1c79475ee719f31dd9bb025e35f8ca
Tomruler <bricechen101@gmail.com>
2024-04-01 02:13:14 -0500

    Removed older commented code from widget file

--- beyond_all_buttplug.lua
+++ beyond_all_buttplug.lua
@@ -232,30 +232,6 @@ local function init_event_metrics()
 end
 
 local queuedCommands = {}
---#region Old1
--- local function createPlayerTable()
---     for _, team in ipairs(teamList) do
---         local playerName
---         local _, leader, _, ai = GetTeamInfo(team)
---         if leader then
---             if ai then
---                 _, playerName = GetAIInfo(team)
---             else
---                 playerName = GetPlayerInfo(leader)
---             end
---             if not playerName then
---                 playerName = "Player Not Found"
---             end
---         end
---         playerTable[team] = playerName
---     end
--- end
-
--- local function lines_from(file)
---     if not file then return {} end
-
--- end
---#endregion
 local function append_queued_commands_to_file()
     if #queuedCommands == 0 then return end
     Spring.Echo(#queuedCommands .. " commands waiting to be written.")
@@ -271,138 +247,6 @@ local function append_queued_commands_to_file()
     end
 end
 
---#region Old2
--- local function checkLock()
---     local file = io.open(globalPath.."lock.txt", "r")
---     if file then
---         file:close()
---     end
--- end
-
--- local function tableToCSV(tbl, name, finalFrame)
---     local file = io.open(globalPath..name, "w")
---     if file then
---         local heading = ""
---         for i = 0, finalFrame, timeInterval do
---             heading = heading .. i/1800 .. ","
---         end
---         if not ((finalFrame%timeInterval)==0) then
---             heading = heading .. finalFrame/1800
---         end
---         for stat, globalData in pairs(tbl) do
---             file:write(stat.."\n")
---             file:write("AllyTeamID,TeamID,Player Name,"..heading.."\n")
---             for i = 0, teamCount - 1 do
---                 local team = i
---                 local data = globalData[team]
---                 local _, _, _, _, _, allyTeamID = GetTeamInfo(team)
-
---                 if playerTable[team] then
---                     local dataString = allyTeamID..","..team..","..playerTable[team] .. ","
---                     for _, val in ipairs(data) do
---                         dataString = dataString .. val .. ","
---                     end
---                     dataString = dataString .. "\n"
---                     file:write(dataString)
---                 end
---             end
---             file:write("\n")
---         end
---         file:close()
---     end
--- end
-
--- local function addStats(hist)
---     hist.damageEfficiency = 0
---     if not (hist.damageReceived == 0) then
---         hist.damageEfficiency = (hist.damageDealt/hist.damageReceived)*100
---     end
-
---     return hist
--- end
-
--- local function createTable()
---     teamCount = 0
---     local dataTable = {}
---     local finalFrame
---     timeInterval = math.ceil(timeInterval/450)
---     for _,teamID in ipairs(teamList) do
---         if teamID ~= GetGaiaTeamID() then
---             local range = GetTeamStatsHistory(teamID)
---             local history = GetTeamStatsHistory(teamID,0,range)
---             if history then
---                 teamCount = teamCount + 1
---                 for i = 1, range, timeInterval do
---                     for stat, val in pairs(addStats(history[i])) do
---                         if not ignoreList[stat] then
---                             local statTable = dataTable[stat]
---                             if statTable then
---                                 local playerStat = dataTable[stat][teamID]
---                                 if playerStat then
---                                     playerStat[#playerStat+1] = val
---                                 else
---                                     statTable[teamID] = {val}
---                                 end
---                             else
---                                 dataTable[stat] = {[teamID] = {val}}
---                             end
---                         end
---                     end
---                 end
---                 if not ((range%timeInterval)==0) then
---                     for stat, val in pairs(history[#history]) do
---                         if stat == "frame" then
---                             finalFrame = val
---                         end
---                         if not ignoreList[stat] then
---                             local playerStat = dataTable[stat][teamID]
---                             if playerStat then
---                                 playerStat[#playerStat+1] = val
---                             end
---                         end
---                     end
---                 end
---                 if not finalFrame then
---                     finalFrame = range
---                 end
---             end
---         end
---     end
---     data = dataTable
---     timeInterval = timeInterval*450
--- end
-
--- local function addCurrentData(force)
---     if ((frame%timeInterval)==0) or force then
---         for _,teamID in ipairs(teamList) do
---             if teamID ~= GetGaiaTeamID() then
---                 local range = GetTeamStatsHistory(teamID)
---                 local history = GetTeamStatsHistory(teamID,0,range)
---                 if history then
---                     teamCount = teamCount + 1
---                     history = history[#history]
-
---                     for stat, val in pairs(addStats(history)) do
---                         if not ignoreList[stat] then
---                             local statTable = data[stat]
---                             if statTable then
---                                 local playerStat = data[stat][teamID]
---                                 if playerStat then
---                                     playerStat[#playerStat+1] = val
---                                 else
---                                     statTable[teamID] = {val}
---                                 end
---                             else
---                                 data[stat] = {[teamID] = {val}}
---                             end
---                         end
---                     end
---                 end
---             end
---         end
---     end
--- end
---#endregion
 local function format_command_string(commandFrame, commandName, commandParams)
     local assembled_command_string = commandFrame.." "
     assembled_command_string = assembled_command_string..commandName.." "
@@ -515,24 +359,7 @@ function widget:GameFrame(n)
     end
     -- Spring.GetTeamUnitsByDefs(0, )
 end
---#region Old3
--- local function createName()
---     local mapName = Game.mapName
---     local timeDate = os.date("%Y-%m-%d_%H-%M".."_"..mapName..".csv")
---     return timeDate
--- end
 
--- local function saveData(fromAction)
---     if isSpec then
---         addCurrentData(true)
---     else
---         createTable()
---         Spring.Echo("Save will be more low resolution")
---     end
---     tableToCSV(data, createName(), frame)
---     Spring.Echo("Resource Data Saved")
--- end
---#endregion
 local function reset_event_file()
     local file, err = io.open(globalPath .. "cmdlog.txt", "w")
     Spring.Echo(err)
